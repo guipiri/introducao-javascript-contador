26db3d10a03ffaebe4a1e84eb83119075b15d18b
guipiri <gui.soliveiras@gmail.com>
2022-01-16 12:24:28 -0300

    nome alterado

--- to-do_list/assets/js/script.js
+++ to-do_list/assets/js/script.js
@@ -1,6 +1,5 @@
 const elemento_pai = document.querySelector("#elemento_pai");
 
-
 function adicionarTarefa () {
   var tarefa = document.querySelector("#tarefa").value;
   var tarefaNode = document.createTextNode(tarefa);
@@ -9,7 +8,7 @@ function adicionarTarefa () {
   var divNova = document.createElement("div");
   var checkBox = document.createElement("input");
   var etiqueta = document.createElement('label');
-
+  
   etiqueta.setAttribute('for', tarefa);
   etiqueta.appendChild(tarefaNode);
 
@@ -23,27 +22,3 @@ function adicionarTarefa () {
 }
 
 
-
-
-// function adicionarTarefa() {
-//   const bloco = document.createElement('div');
-//   const checkBox = document.createElement('input');
-//   const etiqueta = document.createElement('label');
-//   const tarefa = document.querySelector('#tarefa').valeu;
-//   const tarefaNode = document.createTextNode(tarefa);
-
-//   console.log(tarefa)
-
-//   etiqueta.setAttribute('for', tarefa);
-//   etiqueta.appendChild(tarefaNode);
- 
-//   checkBox.setAttribute('type', 'checkbox');
-//   checkBox.setAttribute('id', tarefa);
-//   checkBox.setAttribute('name', tarefa);
-
-//   bloco.appendChild(checkBox);
-//   bloco.appendChild(etiqueta);
-
-//   elemento_pai.appendChild(bloco);
-
-// }
