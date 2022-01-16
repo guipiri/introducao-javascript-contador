2a112328ea70d053dabe6f81727032e0518e02a7
guipiri <gui.soliveiras@gmail.com>
2022-01-16 12:20:02 -0300

    Versão pronta da To-do list

--- to-do_list/assets/js/script.js
+++ to-do_list/assets/js/script.js
@@ -1,41 +1,49 @@
-// var q = 0
-
-// function adicionarTarefa () {
-//   var tarefa = document.querySelector("#tarefa").value;
-//   var elemento_pai = document.querySelector("#elemento_pai");
-//   var pNovo = document.createElement("p");
-//   var checkBox = document.createElement("input");
-
-//   pNovo.id = "p" + q;
-//   checkBox.id = "c" + q;
-
-//   checkBox.type = "checkbox";
-//   pNovo.appendChild(checkBox);
-//   pNovo.append(tarefa);
-//   elemento_pai.appendChild(pNovo);
-//   q++;
-}
 const elemento_pai = document.querySelector("#elemento_pai");
 
-function adicionarTarefa() {
- var bloco = document.createElement('div');
- var checkBox = document.createElement('input');
- var etiqueta = document.createElement('label');
- var tarefa = document.getElementById('checkbox').value;
- var tarefaNode = document.createTextNode(tarefa);
 
- console.log(tarefa)
+function adicionarTarefa () {
+  var tarefa = document.querySelector("#tarefa").value;
+  var tarefaNode = document.createTextNode(tarefa);
+  var elemento_pai = document.querySelector("#elemento_pai");
 
- etiqueta.setAttribute('for', tarefa);
- etiqueta.appendChild(tarefaNode);
- 
- checkBox.setAttribute('type', 'checkbox');
- checkBox.setAttribute('id', tarefa);
- checkBox.setAttribute('name', tarefa);
+  var divNova = document.createElement("div");
+  var checkBox = document.createElement("input");
+  var etiqueta = document.createElement('label');
 
- bloco.appendChild(checkBox);
- bloco.appendChild(etiqueta);
+  etiqueta.setAttribute('for', tarefa);
+  etiqueta.appendChild(tarefaNode);
 
- elemento_pai.appendChild(bloco);
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.setAttribute('id', tarefa);
+  
+  divNova.appendChild(checkBox);
+  divNova.append(etiqueta);
 
+  elemento_pai.appendChild(divNova); 
 }
+
+
+
+
+// function adicionarTarefa() {
+//   const bloco = document.createElement('div');
+//   const checkBox = document.createElement('input');
+//   const etiqueta = document.createElement('label');
+//   const tarefa = document.querySelector('#tarefa').valeu;
+//   const tarefaNode = document.createTextNode(tarefa);
+
+//   console.log(tarefa)
+
+//   etiqueta.setAttribute('for', tarefa);
+//   etiqueta.appendChild(tarefaNode);
+ 
+//   checkBox.setAttribute('type', 'checkbox');
+//   checkBox.setAttribute('id', tarefa);
+//   checkBox.setAttribute('name', tarefa);
+
+//   bloco.appendChild(checkBox);
+//   bloco.appendChild(etiqueta);
+
+//   elemento_pai.appendChild(bloco);
+
+// }
